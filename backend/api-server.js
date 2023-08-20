e6e0bedce388e95b1fdfe8a3f74d399f7dc1fb9e
raonPlanet <do.it.raon@gmail.com>
2023-08-20 18:19:49 +0900

    vue3-memo , done

--- backend/api-server.js
+++ backend/api-server.js
@@ -2,25 +2,31 @@ const express = require('express')
 const bodyParser = require("express");
 const app = express()
 const port = 3000
+const database = require("./database");
 
 const memos = [];
 app.use(bodyParser.json());
-app.get('/api/memos', (req, res) => {
-    res.send(memos)
+app.get('/api/memos', async (req, res) => {
+    const result = await database.run("select * from memos");
+    res.send(result)
 });
-app.post('/api/memos', (req, res) => {
+app.post('/api/memos', async (req, res) => {
     console.log(req.body);
-    memos.push(req.body.content);
-    res.send(memos);
+    await database.run(`insert into memos (content) VALUES (?)`,[req.body.content]);
+    const result = await database.run("select * from memos");
+    res.send(result);
 });
-app.put('/api/memos/:idx', (req, res) => {
-
-    memos[req.params.idx] = req.body.content;
-    console.log(req.params.idx);
-    console.log(req.body);
-    res.send(memos);
+app.put('/api/memos/:id', async (req, res) => {
+    console.log(req.params);
+    await database.run(`update memos set content = ? where id = ?`,[req.body.content,req.params.id])
+    const result = await database.run("select * from memos");
+    res.send(result);
 });
-
+app.delete('/api/memos/:id',async (req,res)=>{
+   await database.run("delete from memos where id = ?",[req.params.id]);
+   const result = await database.run("select * from memos")
+    res.send(result);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
